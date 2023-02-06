5571c001de3c9a8a435e44c0cec3f1b04250e72e
Archoza <geofiziks@mail.ru>
2023-02-06 23:51:52 +0300

    add new branch Dev & New images generation network

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js"
 import dalleRoutes from "./routes/dalleRoutes.js"
+import openJourney from "./routes/openJourney.js"
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json({ limit:'50mb' }))
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
+app.use('/api/v1/open-journey', openJourney)
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!')
